ba08d88f893f36f2227adc8597dbbe71263492fd
David Rosillo <drr538@gmail.com>
2021-08-12 17:53:05 -0500

    Update the service provider

--- src/LedgerServiceProvider.php
+++ src/LedgerServiceProvider.php
@@ -5,15 +5,41 @@ namespace Mrjutsu\Ledger;
 use Illuminate\Support\ServiceProvider;
 use Mrjutsu\Ledger\Console\InstallCommand;
 
-class LedgerServiceProvider extends ServiceProvider {
-
+class LedgerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
     public function register()
     {
-        $this->commands(
-            [
-                InstallCommand::class,
-            ]
-        );
+
     }
 
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->configurePublishing();
+    }
+
+    /**
+     * Configure the publishable resources offered by the package.
+     *
+     * @return void
+     */
+    protected function configurePublishing()
+    {
+        if ($this->app->runningInConsole()) {
+
+            $this->publishes([
+                __DIR__.'/../database/migrations' => database_path('migrations'),
+            ], 'ledger-migrations');
+
+        }
+    }
 }
