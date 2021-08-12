689dc170512ae923c5bd18c322db35aca1ce38e2
David Rosillo <drr538@gmail.com>
2021-08-12 18:03:36 -0500

    Add a user relation

--- src/Traits/Loggable.php
+++ src/Traits/Loggable.php
@@ -12,11 +12,16 @@ trait Loggable {
     }
 
     /*
-    * Registers a polymorphic relationship
+    * Registers a polymorphic relationship between the model and LedgerLog
     **/
     public function ledgerLogs()
     {
         return $this->morphMany(LedgerLog::class, 'loggable');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(App\Models\User::class);
     }
 
 }
